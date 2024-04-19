0dfd0d449ea3703fe522d9d50e49ea29761bc8b6
hellocloudnative <200922702@qq.com>
2024-04-19 11:19:22 +0800

    优化所有镜像迁移功能，并发迁移镜像

--- master/allImages.go
+++ master/allImages.go
@@ -7,47 +7,62 @@ import (
 	"github.com/containerd/containerd"
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
+	"sync"
 )
 
-// MigrateAllImages migrates all images from a source runtime to a destination runtime.
+// MigrateAllImages concurrently migrates all images from a source runtime to a destination runtime.
 func MigrateAllImages(srcType, dstType, namespace string) error {
-	// 创建 Docker 客户端
 	dockerCli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
 		return fmt.Errorf("failed to create Docker client: %v", err)
 	}
 	defer dockerCli.Close()
 
-	// 创建 Containerd 客户端
 	containerdCli, err := containerd.New("/run/containerd/containerd.sock", containerd.WithDefaultNamespace(namespace))
 	if err != nil {
 		return fmt.Errorf("failed to create Containerd client: %v", err)
 	}
 	defer containerdCli.Close()
 
-	// 获取所有 Docker 镜像
 	images, err := dockerCli.ImageList(context.Background(), types.ImageListOptions{All: true})
 	if err != nil {
 		return fmt.Errorf("failed to list Docker images: %v", err)
 	}
 
-	// 遍历所有镜像并迁移
+	done := make(chan bool)
+	errs := make(chan error, len(images))
+
+	var wg sync.WaitGroup
+
 	for _, img := range images {
-		for _, tag := range img.RepoTags {
-			if err := MigrateImage(srcType, dstType, tag, namespace); err != nil {
-				logger.Info("Failed to migrate image %s: %v", tag, err)
-			} else {
+		wg.Add(1)
+		go func(img types.ImageSummary) {
+			defer wg.Done()
+			for _, tag := range img.RepoTags {
+				err := MigrateImage(srcType, dstType, tag, namespace)
+				if err != nil {
+					errs <- fmt.Errorf("failed to migrate image %s: %v", tag, err)
+					return
+				}
 				logger.Info("Successfully migrated image %s", tag)
 			}
+			done <- true
+		}(img)
+	}
+
+	go func() {
+		wg.Wait()
+		close(done)
+	}()
+
+	for _ = range images {
+		select {
+		case err := <-errs:
+			return err
+		case <-done:
 		}
 	}
 
-	// 如果所有镜像都成功迁移，则打印成功消息
-	if count := len(images); count == 0 {
-		logger.Info("No images to migrate.")
-	} else {
-		logger.Info("All %d images migrated successfully.", count)
-	}
-
+	logger.Info("All images migrated successfully")
 	return nil
 }
